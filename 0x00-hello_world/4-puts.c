fd5792a7b52dd11292e09683561e16a4fe34c1f0
VictoryChinakwe <villychina@gmail.com>
2023-03-24 15:18:23 -0700

    All Scripts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include <stdio.h>
+/**                                                      
+* main -Writing a program that prints with puts function 
+* Return: 0 (Success)
+*/
+int main(void)
+{
+put("Programming is like building a multilingual puzzle");
+return (0):
+}
