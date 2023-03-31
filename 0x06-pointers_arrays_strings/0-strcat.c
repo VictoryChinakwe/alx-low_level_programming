7e0a619b481ae01c0e159e4d67762998c915e114
VictoryChinakwe <villychina@gmail.com>
2023-03-30 17:42:09 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+void concatenate_strings char *_strcat(char *dest, char *src);
+}
+int main() {
+char string1 =char *dest
+char string2 = char *src
+concatenate_strings (string1, string2);
+printf("%s", string1);  // Output: "String1"
+return 0;
+}
